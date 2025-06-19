085fbcf2eec3d5a4e005d3bf7b9243a69b427a5c
joseimdev <joseimdev@gmail.com>
2025-06-19 15:31:19 +0900

    [fix] Header에 Setting icon 추가

--- components/header/MainHeader.tsx
+++ components/header/MainHeader.tsx
@@ -5,7 +5,10 @@ export default function MainHeader() {
   return (
     <View className="w-[92%] flex-row justify-between items-end h-full pb-2">
       <Text className="font-[Bold] text-2xl text-white">Echo</Text>
-      <Ionicons name="notifications-outline" size={24} color="#ffffff" />
+      <View className="w-fit h-fit flex-row gap-4">
+        <Ionicons name="notifications-outline" size={24} color="#ffffff" />
+        <Ionicons name="settings-outline" size={24} color="#ffffff" />
+      </View>
     </View>
   );
 }
