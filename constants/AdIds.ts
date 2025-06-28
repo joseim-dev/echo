8e4a766eba4ff5d5aee90ae3ee0e4bddec42c199
joseimdev <joseimdev@gmail.com>
2025-06-28 18:42:05 +0900

    [fix] App ID 수정

--- constants/AdIds.ts
+++ constants/AdIds.ts
@@ -6,11 +6,11 @@ import { TestIds } from "react-native-google-mobile-ads";
 export const ChannelAddRewardedAdId = __DEV__
   ? TestIds.REWARDED
   : Platform.OS === "ios"
-    ? "ca-app-pub-7270360511167481~2820892385"
-    : "ca-app-pub-7270360511167481~8815455094";
+    ? "ca-app-pub-7270360511167481/7893233589"
+    : "ca-app-pub-7270360511167481/5267070243";
 
 export const AppOpenAdId = __DEV__
-  ? TestIds.INTERSTITIAL
+  ? TestIds.APP_OPEN
   : Platform.OS === "ios"
-    ? "ca-app-pub-7270360511167481~2820892385"
-    : "ca-app-pub-7270360511167481~8815455094";
+    ? "ca-app-pub-7270360511167481/7049821559"
+    : "ca-app-pub-7270360511167481/5822168668";
